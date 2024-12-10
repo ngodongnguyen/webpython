e8e19c53c17a75d3e536117d56cd9b5400d499d7
Ngo Dong Nguyen <ngodongnguyen27@gmail.com>
2024-12-10 21:36:35 +0700

    hi

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -8,21 +8,21 @@ def create_app():
     app = Flask(__name__)
 
     # Cấu hình kết nối cơ sở dữ liệu MySQL
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Ngodongnguyen2004?@localhost/PhongY'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Ngodongnguyen2004?@localhost/PhongY'
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-    # Khởi tạo SQLAlchemy với app
-    db.init_app(app)
+    # # Khởi tạo SQLAlchemy với app
+    # db.init_app(app)
 
-    # Import models để tạo bảng
-    from myapp.models import NguoiDung, BacSi, BenhNhan, YTa
+    # # Import models để tạo bảng
+    # from myapp.models import NguoiDung, BacSi, BenhNhan, YTa
 
-    # Tạo các bảng trong cơ sở dữ liệu (Chỉ dùng khi phát triển)
-    with app.app_context():
-        db.create_all()  # Tạo bảng từ tất cả các mô hình đã định nghĩa
+    # # Tạo các bảng trong cơ sở dữ liệu (Chỉ dùng khi phát triển)
+    # with app.app_context():
+    #     db.create_all()  # Tạo bảng từ tất cả các mô hình đã định nghĩa
 
-    # Chuyển import vào trong hàm để tránh import vòng
-    from myapp import routes
-    app.register_blueprint(routes.bp)
+    # # Chuyển import vào trong hàm để tránh import vòng
+    # from myapp import routes
+    # app.register_blueprint(routes.bp)
 
     return app
--- myapp/create_db.py
+++ myapp/create_db.py
@@ -2,21 +2,21 @@
 from myapp import create_app, db
 from myapp.models import NguoiDung
 
-app = create_app()
+# app = create_app()
 
-# Tạo tất cả các bảng theo mô hình đã định nghĩa
-with app.app_context():
-    db.create_all()
+# # Tạo tất cả các bảng theo mô hình đã định nghĩa
+# with app.app_context():
+#     db.create_all()
 
-    # Thêm một user mẫu vào cơ sở dữ liệu
-    new_user = NguoiDung(username='john_doe', email='john@example.com')
+#     # Thêm một user mẫu vào cơ sở dữ liệu
+#     new_user = NguoiDung(username='john_doe', email='john@example.com')
 
-    # Lưu đối tượng vào cơ sở dữ liệu
-    db.session.add(new_user)
-    db.session.commit()
+#     # Lưu đối tượng vào cơ sở dữ liệu
+#     db.session.add(new_user)
+#     db.session.commit()
 
-    # Kiểm tra dữ liệu đã được thêm hay chưa
-    users = NguoiDung.query.all()  # Truy vấn tất cả user
-    print("All Users in the Database:")
-    for user in users:
-        print(f"Username: {user.username}, Email: {user.email}")
+#     # Kiểm tra dữ liệu đã được thêm hay chưa
+#     users = NguoiDung.query.all()  # Truy vấn tất cả user
+#     print("All Users in the Database:")
+#     for user in users:
+#         print(f"Username: {user.username}, Email: {user.email}")
